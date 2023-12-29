31e5efcdd7c4d06990909a9448ed91fd6a36cf79
Antonio Lima <antonio55senac@gmail.com>
2023-12-29 15:13:31 +0000

    debbugando form

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,16 @@
 function enviarFormulario() {
-    document.forms();
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let departamento = document.getElementById('departamento').value;
+    let solicitacao = document.getElementById('solicitação').value;
+
+    console.log(nome);
+    console.log(email);
+    console.log(telefone);
+    console.log(departamento);
+    console.log(solicitacao);
 }
+
+enviarFormulario();
