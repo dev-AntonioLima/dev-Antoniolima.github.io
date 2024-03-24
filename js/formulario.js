32a70b7cd7173f3d196aeba2bd12d132fb79c540
Antonio Lima <antonio55senac@gmail.com>
2024-03-24 16:21:04 +0000

    Validador telefone

--- js/formulario.js
+++ js/formulario.js
@@ -9,8 +9,21 @@ nome.addEventListener("keypress", function(e) {
 });
 
 
+// VALIDAÇÃO CAMPO TELEFONE //
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
 
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
 
+  
 
 function enviarFormulario() {
 
